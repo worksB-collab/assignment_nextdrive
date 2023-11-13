23f4465d60ba763201befd98d70a27f978f9fc87
Billy Wu <billy31103@gmail.com>
2023-11-13 16:02:40 +0800

    Initial commit

---
+++ src/test/kotlin/com/example/assignment_nextdrive/AssignmentNextdriveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.assignment_nextdrive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssignmentNextdriveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
